65cb2ff1fe08fae1a630f22fe4bacfab52e3190d
Ben Baumer <ben.baumer@gmail.com>
2015-08-04 15:37:14 -0400

    added MedicareChargees and MedicareProviders

--- data-raw/CountryData.R
+++
@@ -1,4 +0,0 @@
-url <- "https://github.com/DataComputing/DataComputing/raw/master/data/CountryData.rda"
-download.file(url, "data/CountryData.rda")
-
-download.file("https://raw.githubusercontent.com/DataComputing/DataComputing/master/man/CountryData.Rd", "man/CountryData.Rd")
---
+++ data-raw/DataComputing.R
@@ -0,0 +1,12 @@
+# CountryData
+download.file("https://github.com/DataComputing/DataComputing/raw/master/data/CountryData.rda", "data/CountryData.rda")
+download.file("https://raw.githubusercontent.com/DataComputing/DataComputing/master/man/CountryData.Rd", "man/CountryData.Rd")
+
+# MedicareCharges
+download.file("https://github.com/DataComputing/DataComputing/raw/master/data/MedicareCharges.rda", "data/MedicareCharges.rda")
+download.file("https://raw.githubusercontent.com/DataComputing/DataComputing/master/man/MedicareCharges.Rd", "man/MedicareCharges.Rd")
+
+# MedicareProviders
+download.file("https://github.com/DataComputing/DataComputing/raw/master/data/MedicareProviders.rda", "data/MedicareProviders.rda")
+download.file("https://raw.githubusercontent.com/DataComputing/DataComputing/master/man/MedicareProviders.Rd", "man/MedicareProviders.Rd")
+
