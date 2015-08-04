a1ceb5e5dc4f1b59028f5ffae9d95fe5ec5db9f3
Ben Baumer <ben.baumer@gmail.com>
2015-08-04 15:28:46 -0400

    added CountryData

---
+++ data-raw/CountryData.R
@@ -0,0 +1,4 @@
+url <- "https://github.com/DataComputing/DataComputing/raw/master/data/CountryData.rda"
+download.file(url, "data/CountryData.rda")
+
+download.file("https://raw.githubusercontent.com/DataComputing/DataComputing/master/man/CountryData.Rd", "man/CountryData.Rd")
