5f8b43a700b03a04be61a7b77c6670a51e23468e
Ben Baumer <ben.baumer@gmail.com>
2015-10-07 11:33:10 -0400

    added cherry blossoms data

--- data-raw/electionStatistics.R
+++ data-raw/electionStatistics.R
@@ -5,3 +5,17 @@ electionStatistics <- read.csv(url)
 electionStatistics <- as.tbl(read.csv("data-raw/electionStatistics.csv"))
 
 save(electionStatistics, file = "data/electionStatistics.rda", compress = "xz")
+
+
+
+url <- "https://raw.githubusercontent.com/dtkaplan/DCF-2015/master/Book/Sections/Data/Cherry-Blossom-Longitudinal.csv"
+
+cherry <- read.csv(url)
+
+cherry <- as.tbl(read.csv("data-raw/cherry.csv"))
+
+save(cherry, file = "data/cherry.rda", compress = "xz")
+
+
+
+
