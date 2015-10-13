09178f6242f906a2c5e1ffd23fc23635145171ff
Ben Baumer <ben.baumer@gmail.com>
2015-10-13 14:57:46 -0400

    added mysql_scidb

--- R/scidb.R
+++ R/scidb.R
@@ -26,7 +26,7 @@ src_scidb <- function(dbname, ...) {
             user = "mth292", password = "RememberPi")
 }
 
-#' dbConnect_scidb
+#' Create a DBI connection to the scidb server at Smith
 #' 
 #' @rdname src_scidb
 #' @export
@@ -45,4 +45,23 @@ src_scidb <- function(dbname, ...) {
 dbConnect_scidb <- function(dbname, ...) {
   dbConnect(MySQL(), dbname = dbname, host = "scidb.smith.edu", 
             user = "mth292", password = "RememberPi")
+}
+
+#' Create a string of arguments for the scidb server at Smith to be used on the
+#' command line
+#' 
+#' @rdname src_scidb
+#' @export
+#' 
+#' @return For \code{\link{mysql_scidb}}, a charcter vector of length 1 to be used
+#' as an \code{engine.ops} argument, or on the command line. 
+#' 
+#' @seealso \code{\link[knitr]{opts_chunk}}
+#' @examples 
+#' 
+#' library(knitr)
+#' opts_chunk$set(engine.opts = mysql_scidb("airlines"))
+
+mysql_scidb <- function(dbname, ...) {
+  paste("-h scidb.smith.edu -u mth292 -pRememberPi -t", dbname)
 }
