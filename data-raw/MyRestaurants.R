e592873073bf39379370f8dac2cf07c96c6ba5e0
beanumber <ben.baumer@gmail.com>
2015-12-30 17:11:44 -0500

    fixed non-ASCII warning

--- data-raw/MyRestaurants.R
+++ data-raw/MyRestaurants.R
@@ -5,6 +5,17 @@ load("~/Dropbox/lib/mdsr/data/MyRestaurants.rda")
 require(dplyr)
 Cuisines <- as.tbl(Cuisines)
 names(Cuisines) <- c("cuisine_code", "cuisine_description")
+# set encoding for non-ASCII character
+tools::showNonASCII(Cuisines$cuisine_description)
+x <- as.character(Cuisines$cuisine_description)
+Encoding(x) <- "latin1"
+#y <- iconv(x, from = "latin1", to = "ASCII", sub = "e")
+Cuisines$cuisine_description <- x
+
+# note that other columns have UTF characters
+bad <- apply(Restaurants, MARGIN = 2, tools::showNonASCII)
+str(bad)
+
 names(ViolationCodes) <- c("violation_code", "critical_flag", "violation_description")
 Restaurants <- as.tbl(Restaurants)
 names(Restaurants) <- c("camis", "DBA", "boro", "building"
