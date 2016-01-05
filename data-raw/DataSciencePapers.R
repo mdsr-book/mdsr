cee0915ab95b03cee47c39d1654de6fcf8a25ee1
Ben Baumer <ben.baumer@gmail.com>
2016-01-05 09:56:14 -0500

    made DataSciencePapers a tbl

--- data-raw/DataSciencePapers.R
+++ data-raw/DataSciencePapers.R
@@ -1,6 +1,7 @@
 # DataSciencePapers
 
 library(aRxiv)
-DataSciencePapers <- arxiv_search(query = '"Data Science"', limit = 200)
+library(dplyr)
+DataSciencePapers <- as.tbl(arxiv_search(query = '"Data Science"', limit = 200))
 
 save(DataSciencePapers, file = "data/DataSciencePapers.rda", compress = "xz")
