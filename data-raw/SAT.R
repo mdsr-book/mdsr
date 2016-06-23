367b2c9dd20aac18d7989aab758ad378d98f4ba2
Ben Baumer <ben.baumer@gmail.com>
2016-06-23 14:28:50 -0400

    close #13

--- data-raw/SAT.R
+++ data-raw/SAT.R
@@ -1,4 +1,9 @@
 # SAT
 
 SAT_2010 <- read.csv("inst/extdata/GuberSATupdated2010.csv")
+names(SAT_2010) <- names(SAT_2010) %>% 
+  tolower() %>%
+  gsub("\\.", "_", x = .)
+SAT_2010 <- SAT_2010 %>%
+  rename(sat_pct = percentsat)
 save(SAT_2010, file = "data/SAT_2010.rda", compress = "xz")
