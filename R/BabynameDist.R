8fc89c3053c45af3d4ee866d389ed52c2d27c573
Ben Baumer <ben.baumer@gmail.com>
2016-08-17 15:54:16 -0400

    removed dplyr import; close #15

--- R/BabynameDist.R
+++ R/BabynameDist.R
@@ -1,5 +1,5 @@
 #' Wrangle babynames data
-#' @import dplyr
+#' @importFrom dplyr mutate_ select_ bind_rows rename_ inner_join %>%
 #' @import babynames
 #' @importFrom stats approx
 #' @export
@@ -14,32 +14,32 @@
 
 make_babynames_dist <- function() {
   actuarial <- babynames::lifetables %>%
-    mutate_(age_today = ~x, 
+    dplyr::mutate_(age_today = ~x, 
             alive_prob = ~lx / 100000, 
             more_years = ~ex, 
             life_exp = ~x + ex) %>%
-    filter_(~year + x == 2014) %>%
-    select_(~year, ~sex, ~age_today, ~alive_prob, ~life_exp)
+    dplyr::filter_(~year + x == 2014) %>%
+    dplyr::select_(~year, ~sex, ~age_today, ~alive_prob, ~life_exp)
   
   # Unfortunately, the actuarial tables only contain entries for people born each decade, not each year. Following FiveThirtyEight's lead we will linearly interpolate the missing values using the \func{approx} function. 
   
   years <- min(actuarial$year):max(actuarial$year)
   men <- actuarial %>%
-    filter_(~sex == "M") %>%
+    dplyr::filter_(~sex == "M") %>%
     approximate(years = years)
   women <- actuarial %>%
-    filter_(~sex == "F") %>%
+    dplyr::filter_(~sex == "F") %>%
     approximate(years = years)
-  actuarial_interp <- bind_rows(men, women)
+  actuarial_interp <- dplyr::bind_rows(men, women)
   actuarial_interp <- actuarial_interp %>%
-    rename_(year = ~x, alive_prob = ~y) %>%
-    mutate_(sex = ~c(rep("M", length(years)), rep("F", length(years))))
+    dplyr::rename_(year = ~x, alive_prob = ~y) %>%
+    dplyr::mutate_(sex = ~c(rep("M", length(years)), rep("F", length(years))))
   
   # Finally, we will construct the data set that we need by joining the actuarial information with the baby names data. 
   
   BabynameDist <- babynames::babynames %>%
-    inner_join(actuarial_interp, by = c("year", "sex")) %>%
-    mutate_(count_thousands = ~n / 1000, 
+    dplyr::inner_join(actuarial_interp, by = c("year", "sex")) %>%
+    dplyr::mutate_(count_thousands = ~n / 1000, 
             age_today = ~2014 - year, 
             est_alive_today = ~n * alive_prob)
   
