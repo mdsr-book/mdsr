a6ef9dc2e9b1c70c829c8342abbd7eb61df724fc
Ben Baumer <ben.baumer@gmail.com>
2016-08-17 15:38:30 -0400

    added theme and removed dummy

---
+++ R/themes.R
@@ -0,0 +1,29 @@
+#' MDSR themes
+#' 
+#' @description Graphical themes used in MDSR book
+#' 
+#' @inheritParams ggplot2::theme_grey
+#' @import ggplot2
+#' @import mosaic
+#' @export
+#' @examples 
+#' 
+#' p <- ggplot(mtcars, aes(x = hp, y = mpg, color = factor(cyl))) + 
+#'   geom_point() + facet_wrap(~ am) + geom_smooth()
+#' p + theme_grey()
+#' p + theme_mdsr()
+
+theme_mdsr <- function(base_size = 12, base_family = "Bookman") {
+  m <- mosaic::theme_map()
+  ggplot2::theme_grey(base_size = base_size, base_family = base_family) %+replace%
+    ggplot2::theme(
+      axis.text        = ggplot2::element_text(size = ggplot2::rel(0.8)),
+      axis.ticks       = ggplot2::element_line(colour = "black"), 
+      legend.key       = ggplot2::element_rect(colour = "grey80"),
+      panel.background = ggplot2::element_rect(fill = "whitesmoke", colour = NA),
+      panel.border     = ggplot2::element_rect(fill = NA, colour = "grey50"),
+      panel.grid.major = ggplot2::element_line(colour = "grey80", size = 0.2),
+      panel.grid.minor = ggplot2::element_line(colour = "grey92", size = 0.5),
+      strip.background = ggplot2::element_rect(fill = "grey80", colour = "grey50", size = 0.2)
+    )
+}
--- R/utils.R
+++
@@ -1,8 +0,0 @@
-#' dummy
-#' @description dummy function to keep track of dependencies
-#' 
-#' @import mosaic
-#' 
-dummy <- function() {
-  mosaic::do()
-}
