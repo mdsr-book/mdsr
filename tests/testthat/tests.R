6dbb4fcec903acc40167659c253c862aeacba8d7
beanumber <ben.baumer@gmail.com>
2018-06-11 10:20:37 -0400

    beefed up tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -3,4 +3,16 @@ context("mdsr")
 test_that("scidb works", {
   x <- src_scidb("airlines")
   expect_s3_class(x, "src_sql")
+  expect_output(print(x), "mdsr_public")
+  expect_output(print(x), "rds.amazonaws.com")
+  suppressWarnings(
+    expect_equal(
+      x %>% 
+        tbl("airports") %>% 
+        head(1) %>%
+        collect() %>%
+        nrow(), 
+      1
+    )
+  )
 })
